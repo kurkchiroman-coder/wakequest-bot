5abe02cc6d6690a6db2d20d9d5584df3049e5ffe
kurkchiroman-coder <kurkchi.roman@gmail.com>
2026-05-05 17:59:05 +0300

    Add files via upload

---
+++ tricks.js
@@ -0,0 +1,93 @@
+module.exports = [
+
+  // 10 БАЛІВ
+  { id: "stand_board", name: "Встати на дошку", points: 10 },
+  { id: "ride_cable", name: "Поїхати під канатом", points: 10 },
+  { id: "turn_basic", name: "Зробити поворот", points: 10 },
+
+  { id: "one_handed_riding", name: "One-handed riding", points: 10 },
+  { id: "switch_riding", name: "Switch riding", points: 10 },
+  { id: "surface_180", name: "Surface 180", points: 10 },
+  { id: "surface_360", name: "Surface 360", points: 10 },
+  { id: "ollie", name: "Ollie", points: 10 },
+  { id: "nose_tail_press", name: "Nose / Tail Press", points: 10 },
+  { id: "ollie_180", name: "180 Hop / Ollie 180", points: 10 },
+  { id: "ollie_360", name: "360 Hop / Ollie 360", points: 10 },
+  { id: "blind_riding", name: "Blind Riding", points: 10 },
+
+  // 50 БАЛІВ (flat + air)
+  { id: "straight_air", name: "Straight Air", points: 50 },
+  { id: "grab", name: "Grab", points: 50 },
+  { id: "hs_180", name: "HS 180 (Heelside)", points: 50 },
+  { id: "hs_360", name: "HS 360", points: 50 },
+
+  // 50 БАЛІВ (rails)
+  { id: "fifty_fifty", name: "50-50", points: 50 },
+  { id: "fs_boardslide", name: "FS Boardslide", points: 50 },
+  { id: "bs_boardslide", name: "BS Boardslide", points: 50 },
+  { id: "tail_press", name: "Tail Press", points: 50 },
+  { id: "nose_press", name: "Nose Press", points: 50 },
+
+  // 80 БАЛІВ (basic air tricks)
+  { id: "raley", name: "Raley", points: 80 },
+  { id: "backroll", name: "Backroll", points: 80 },
+  { id: "frontflip", name: "Frontflip", points: 80 },
+  { id: "s_bend", name: "S-Bend", points: 80 },
+  { id: "backroll_revert", name: "Backroll to Revert", points: 80 },
+  { id: "krypt", name: "Krypt", points: 80 },
+  { id: "tantrum", name: "Tantrum", points: 80 },
+  { id: "toeside_backroll", name: "Toeside Backroll", points: 80 },
+
+  // 80 БАЛІВ (rotations)
+  { id: "180_out", name: "180 Out", points: 80 },
+  { id: "pre_180_in", name: "Pre-spin 180 In", points: 80 },
+  { id: "270_out", name: "270 Out", points: 80 },
+  { id: "360_out", name: "360 Out", points: 80 },
+  { id: "pre_360_in", name: "Pre-spin 360 In", points: 80 },
+
+  // 100 БАЛІВ (advanced combos)
+  { id: "blind_judge", name: "Blind Judge", points: 100 },
+  { id: "s_bend_to_blind", name: "S-Bend to Blind", points: 100 },
+  { id: "three_thirteen", name: "Three-Thirteen", points: 100 },
+  { id: "kgb", name: "KGB", points: 100 },
+  { id: "kicker_720", name: "Kicker 720", points: 100 },
+  { id: "tantrum_to_blind", name: "Tantrum to Blind", points: 100 },
+  { id: "moby_dick", name: "Moby Dick", points: 100 },
+  { id: "backroll_to_blind", name: "Backroll to Blind", points: 100 },
+  { id: "kicker_900", name: "Kicker 900", points: 100 },
+  { id: "double_tantrum", name: "Double Tantrum", points: 100 },
+  { id: "double_backroll", name: "Double Backroll", points: 100 },
+  { id: "kicker_1080", name: "Kicker 1080", points: 200 },
+
+  // rails advanced
+  { id: "side_entry", name: "Side Entry", points: 100 },
+  { id: "lipslide", name: "Lipslide", points: 100 },
+  { id: "switch_lipslide", name: "Switch Lipslide", points: 100 },
+  { id: "gap_to_rail", name: "Gap to Rail", points: 100 },
+  { id: "blunt_slide", name: "Blunt Slide", points: 100 },
+  { id: "nose_blunt", name: "Nose Blunt Slide", points: 100 },
+  { id: "pretzel", name: "Pretzel", points: 100 },
+  { id: "sex_change", name: "Sex Change", points: 100 },
+  { id: "bs_450_out", name: "BS 450 Out", points: 100 },
+  { id: "blind_180_out", name: "Blind 180 Out", points: 100 },
+  { id: "press_180_out", name: "Press to 180 Out", points: 100 },
+  { id: "tumble_turn_off", name: "Tumble Turn Off", points: 100 },
+  { id: "shifty_to_slide", name: "Shifty to Slide", points: 100 },
+
+  // 100 БАЛІВ (pro air)
+  { id: "double_half_cab_roll", name: "Double Half-Cab Roll", points: 100 },
+  { id: "whirlybird_540", name: "Whirlybird 540", points: 100 },
+  { id: "double_scarecrow", name: "Double Scarecrow", points: 100 },
+  { id: "tail_grab_to_nose_press", name: "Tail Grab to Nose Press", points: 100 },
+  { id: "crail_slide", name: "Crail Slide", points: 100 },
+  { id: "sw_270_combo", name: "Sw 270 In to 270 Out", points: 100 },
+
+  // 300 БАЛІВ (PRO LEVEL)
+{ id: "double_backroll_moby", name: "Double Backroll + Moby Dick", points: 300 },
+{ id: "heater_double_combo", name: "Heater Combo (Extended)", points: 300 },
+{ id: "double_half_cab_extreme", name: "Double Half-Cab Roll EXT", points: 300 },
+{ id: "whirlybird_720_combo", name: "Whirlybird 720 Combo", points: 300 },
+{ id: "double_scarecrow_ext", name: "Double Scarecrow Extended", points: 300 },
+{ id: "kicker_1080_combo_pro", name: "Kicker 1080 Pro Combo", points: 300 },
+
+]
